2e3fca3defa3b38b60a4f6aa478936899ca853bc
Júlia Noveline Britto <106248989+julianoveline015@users.noreply.github.com>
2025-10-25 16:38:32 -0300

    Add files via upload

---
+++ Exemplo02.py
@@ -0,0 +1,11 @@
+import os
+os.system('clear')
+
+texto= "Wingardium Leviosa"
+print(texto.lower())
+print(texto.upper())
+textoAlterado = texto.replace("Wingardium Leviosa", "Jacob")
+print(textoAlterado.upper())
+
+texxto = texto[::-1]
+print(texxto)
---
+++ Exemplos.py
@@ -0,0 +1,11 @@
+import os
+os.system('clear')
+
+nome = "Wingardium Leviosa"
+
+print(len(nome))
+print(nome[::-1])
+print(nome[4])
+print(nome[2::])
+print(nome[:3:])
+print(nome[:-1])
